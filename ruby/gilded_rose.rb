164b61117b708a6a476e397bd7d4fd833637dad5
Sam Belmor <sambelmor@gmail.com>
2020-06-21 18:47:31 -0500

    GildedRose Refactoring Kata

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,54 +1,75 @@
-class GildedRose
+# frozen_string_literal: true
 
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+      if different_from_brie_and_backstage?(item.name)
+        item.quality = first_validation(item)
+      elsif quality_less_than_fifty?(item.quality)
+        second_validation(item)
+      elsif different_from_sulfuras?(item.name)
+        item.sell_in = subtract(item.sell_in)
+      elsif item.sell_in.negative?
+        third_validation(item)
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+    end
+  end
+
+  def different_from_sulfuras?(name)
+    name != 'Sulfuras, Hand of Ragnaros'
+  end
+
+  def diffent_from_brie?(name)
+    name != 'Aged Brie'
+  end
+
+  def different_from_brie_and_backstage?(name)
+    diffent_from_brie?(name) &&
+      name != 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  def equal_to_brie_and_backstage(name)
+    name == 'Backstage passes to a TAFKAL80ETC concert'
+  end
+
+  def subtract(item)
+    item - 1
+  end
+
+  def sum(quality)
+    quality + 1
+  end
+
+  def quality_less_than_fifty?(quality)
+    quality < 50
+  end
+
+  def first_validation(item)
+    return unless item.quality.positive? && different_from_sulfuras?(item.name)
+
+    subtract(item.quality)
+  end
+
+  def second_validation(item)
+    item.quality = sum(item.quality)
+    item.quality = if equal_to_brie_and_backstage(item.name) && item.sell_in < 11
+                     sum(item.quality)
+                   elsif item.sell_in < 6
+                     sum(item.quality)
+                   end
+  end
+
+  def third_validation(item)
+    if different_from_brie_and_backstage?(item.name)
+      first_validation(item)
+    elsif diffent_from_brie?(item.name)
+      item.quality = item.quality - item.quality
+    elsif quality_less_than_fifty?(item.quality)
+      sum(item.quality)
     end
   end
 end
@@ -62,7 +83,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -1,13 +1,47 @@
+# frozen_string_literal: true
+
+require 'pry'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
+  describe '#update_quality' do
+    it 'does not change the name' do
+      items = [Item.new('First item', 0, 0)]
+      updated_item = GildedRose.new(items).update_quality
 
-  describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(updated_item.first.name).to eq 'First item'
+    end
+
+    it 'should decrease the quality by one' do
+      items = [Item.new('First item', 20, 15)]
+      updated_item = GildedRose.new(items).update_quality
+
+      expect(updated_item.first.quality).to eq(14)
+      expect(updated_item.first.sell_in).to eq(20)
+    end
+
+    it 'should increase two into the quality' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 35)]
+      updated_item = GildedRose.new(items).update_quality
+
+      expect(updated_item.first.quality).to eq(37)
+      expect(updated_item.first.sell_in).to eq(10)
+    end
+
+    it 'should increase the quality value by 2' do
+      items = [Item.new('Aged Brie', 5, 20)]
+      updated_item = GildedRose.new(items).update_quality
+
+      expect(updated_item.first.quality).to eq(22)
+      expect(updated_item.first.sell_in).to eq(5)
+    end
+
+    it 'should decrease the quantity in sell in by one' do
+      items = [Item.new('Aged Brie', 2, 100)]
+      updated_item = GildedRose.new(items).update_quality
+
+      expect(updated_item.first.quality).to eq(100)
+      expect(updated_item.first.sell_in).to eq(1)
     end
   end
-
 end
